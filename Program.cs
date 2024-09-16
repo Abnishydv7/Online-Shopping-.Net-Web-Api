6b93b3d05c3c4ad94f9784b3a6740e1bf9efb1bf
Abnish kumar yadav <yadavabnishkumar7@gmail.com>
2024-09-16 08:14:57 +0530

    Add files via upload
    This is my first project using c# language and .net framework. In this project i used purely .net, sql, ssms any many more realted to .net core. here are the source code available here.....
    This is very useful for backend developer.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineShopping_ELX.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<OnlineShopping_ELXContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("OnlineShopping_ELXContext") ?? throw new InvalidOperationException("Connection string 'OnlineShopping_ELXContext' not found.")));
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+  .AddCookie(options => options.LoginPath = new PathString("/User/Login"));
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=product}/{action=ProductList}/{id?}");
+
+app.Run();
